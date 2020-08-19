e5572f58fca7859a0514062bbbcbd4b3a0158dee
sithsiri <sith@live.com>
2020-08-18 23:45:57 -0500

    more modular random script

--- randomScript.js
+++ randomScript.js
@@ -1,13 +1,10 @@
-window.onload = function run() {
-  switch(Math.floor(Math.random() * 3)) {
-    case 0:
-      document.getElementById("randomFact").innerHTML = "Many of our members don't actually code.";
-      break;
-    case 1:
-      document.getElementById("randomFact").innerHTML = "RRHS Code Club is not sponsored by Pepsi.";
-      break;
-    case 2:
-      document.getElementById("randomFact").innerHTML = "Rust and its Cargo compiler will one day be the savior of humanity.";
-      break;
-  }
+let facts = [
+	"Many of our members don't actually code.",
+	"RRHS Code Club is not sponsored by Pepsi.",
+	"Rust and its Cargo compiler will one day be the savior of humanity.",
+	"RRHS Code Club may or may not be in the process of taking over the Tri-State Area."
+];
+
+window.onload = function new_fact() {
+	document.getElementById("randomFact").innerHTML = facts[Math.floor(Math.random() * facts.length)];
 }
