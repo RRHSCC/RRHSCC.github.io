5f174743c455b540ed41fa70157cf8c97594e52d
sithsiri <sith@live.com>
2020-08-18 22:36:44 -0500

    maybe it will actually work now

--- randomScript.js
+++ randomScript.js
@@ -1,3 +1,13 @@
-if(Math.floor(Math.random()+0.5) == 1){
-  document.getElementById("randomFact").innerHTML = "Almost half of our members don't actually code.";
+window.onload = function run() {
+  switch(Math.floor(Math.random() * 3)) {
+    case 0:
+      document.getElementById("randomFact").innerHTML = "Many of our members don't actually code.";
+      break;
+    case 1:
+      document.getElementById("randomFact").innerHTML = "RRHS Code Club is not sponsored by Pepsi.";
+      break;
+    case 2:
+      document.getElementById("randomFact").innerHTML = "Rust and its Cargo compiler will one day be the savior of humanity.";
+      break;
+  }
 }
